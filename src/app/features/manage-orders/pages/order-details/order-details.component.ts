601850432c2242039782e0822818323b19ff6872
niraj <niraj.kumar@plexcel.com>
2021-01-30 02:43:57 +0530

    Implemented order details

--- src/app/features/manage-orders/pages/order-details/order-details.component.ts
+++ src/app/features/manage-orders/pages/order-details/order-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ManageOrdersService } from '../../manage-orders.service';
+import { IOrders } from '../../models/orders.model';
 
 @Component({
   selector: 'app-order-details',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-details.component.scss']
 })
 export class OrderDetailsComponent implements OnInit {
-
-  constructor() { }
+  order: IOrders = null;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private manageOrderService: ManageOrdersService
+  ) { }
 
   ngOnInit() {
+    const id = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.getOrderById(id);
+  }
+
+  getOrderById(id: number) {
+    this.manageOrderService.getOrderById(id)
+      .subscribe((order) => { this.order = order}, (err) => { alert('something went wrong') });
   }
 
 }
