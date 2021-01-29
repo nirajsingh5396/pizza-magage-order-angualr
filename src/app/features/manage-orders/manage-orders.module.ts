2df486c7d496831cb15d230d34cff33c04189f39
niraj <niraj.kumar@plexcel.com>
2021-01-30 02:21:12 +0530

    Created order details component

--- src/app/features/manage-orders/manage-orders.module.ts
+++ src/app/features/manage-orders/manage-orders.module.ts
@@ -5,11 +5,12 @@ import { ManageOrdersRoutingModule } from './manage-order-routing.module';
 import { OrderReceivedComponent } from './pages/order-received/order-received.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ReceivedItemComponent } from './pages/received-item/received-item.component';
+import { OrderDetailsComponent } from './pages/order-details/order-details.component';
 
 
 
 @NgModule({
-  declarations: [MangeOrdersComponent, OrderReceivedComponent, ReceivedItemComponent],
+  declarations: [MangeOrdersComponent, OrderReceivedComponent, ReceivedItemComponent, OrderDetailsComponent],
   imports: [
     CommonModule,
     ManageOrdersRoutingModule,
