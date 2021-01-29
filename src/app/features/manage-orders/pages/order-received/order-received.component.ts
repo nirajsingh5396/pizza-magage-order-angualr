d31edda17b0882dd00bdccef79986045bace023c
niraj <niraj.kumar@plexcel.com>
2021-01-29 23:13:52 +0530

    Implemented order sttaus in drop down and listed all status

--- src/app/features/manage-orders/pages/order-received/order-received.component.ts
+++ src/app/features/manage-orders/pages/order-received/order-received.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ManageOrdersService } from '../../manage-orders.service';
 
 @Component({
   selector: 'app-order-received',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-received.component.scss']
 })
 export class OrderReceivedComponent implements OnInit {
+  items = [11, 2, 3, 4, 5]
+  orderStatus$: Observable<string[]>;
 
-  constructor() { }
+  constructor(private manageOrderService: ManageOrdersService) { }
 
   ngOnInit() {
+    this.getOrderStatus();
+  }
+
+  getOrderStatus() {
+    this.orderStatus$ = this.manageOrderService.getOrderStatus();
   }
 
 }
