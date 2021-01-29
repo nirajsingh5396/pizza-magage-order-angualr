0a7be8c0e7a78e7db43ad0e7808fdf3002a08590
niraj <niraj.kumar@plexcel.com>
2021-01-29 16:09:18 +0530

    Created lazy loaded route for layout module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'layout',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'layout',
+    loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule)
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
