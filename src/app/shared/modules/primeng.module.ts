cdf2c342d42403361022e0e3aaecc7daf7f8bf96
niraj <niraj.kumar@plexcel.com>
2021-01-31 00:30:06 +0530

    Implemented notification service

--- src/app/shared/modules/primeng.module.ts
+++ src/app/shared/modules/primeng.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from "@angular/core";
 
 
-import {ButtonModule} from 'primeng/button';
-import {OrderListModule} from 'primeng/orderlist';
+import { ButtonModule } from 'primeng/button';
+import { OrderListModule } from 'primeng/orderlist';
+import { TooltipModule } from 'primeng/tooltip'
 
 
 
 
 @NgModule({
-    exports: [ButtonModule , OrderListModule]
+    exports: [ButtonModule, OrderListModule, TooltipModule]
 })
 
-export class PrimeNgModule{}
+export class PrimeNgModule { }
