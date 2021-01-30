bfdb86e362a016095466c569ebcc0410f3c6d48c
niraj <niraj.kumar@plexcel.com>
2021-01-30 21:24:07 +0530

    Implemented new design of received orders

--- src/app/shared/modules/primeng.module.ts
+++ src/app/shared/modules/primeng.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 
 
 import {ButtonModule} from 'primeng/button';
+import {OrderListModule} from 'primeng/orderlist';
+
 
 
 
 @NgModule({
-    exports: [ButtonModule]
+    exports: [ButtonModule , OrderListModule]
 })
 
 export class PrimeNgModule{}
